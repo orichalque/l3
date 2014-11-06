4b3ec63b228e1282fb9dc4e610ee1de5b95285a9
Dennis BORDET <E129986P@I019V7pc04.ensinfo.sciences.univ-nantes.prive>
2014-11-06 18:53:21 +0100

    Node class updated

--- Proj_Algo/Noeud.java
+++ Proj_Algo/Noeud.java
@@ -7,7 +7,8 @@ public class Noeud<E extends Comparable<E>> {
 	protected int bal;
 	
 	//CONSTRUCTEUR
-	public Noeud(E base)
+	//public Noeud<E>();
+	public Noeud<E>(E base)
 	{
 		etq = base;
 		nbFils=0;
@@ -46,37 +47,40 @@ public class Noeud<E extends Comparable<E>> {
 		return (leSon != null);
 	}
 	
-	public int addSon(Noeud<E> n) {
-		if (n = null) {
-			
-			//incomplet
+	public int addSon(E x, int variationH) { // on ajoute sur un arbre non vide => constructeur non vide 
+		int h;
+		if(this == null){
+			Noeud<E> B = new Noeud(x);
+			this = B;
+			return 1;
+		}else{
+			if(x!=etq){
+				if ( x > etq ){
+					h = riSon.addSon(x);
+				}else{
+					h = leSon.addSon(x);
+					h = -h;
+					if(h == 0){
+						return 0;
+					}else{
+						bal = bal + h;
+						equilibrer();
+						if(bal == 0){
+							return 0;
+						}else{
+							return 1;
+						}
+					}
+				}
+			}else{
+				return 0;
+			}
+		
+		
 		}
 	}
 	
-
-	public Noeud<E> ROTG() {
-		Noeud<E> k = riSon; //b <- fils droit
-	public void ROTG() {
-		Noeud k = riSon; //b <- fils droit
-		int a = balance(); 
-		int b = k.balance();
-		riSon = k.getLeSon();
-		k.leSon = this; /*Rotation*/
-		setBal(a-Math.max(b, 0)-1);
-		k.setBal(Math.min(a-2, Math.min(a+b, b-1))); //La noeud droit de l'objet est devenu racine.
-		return k;
-	}
 	
-	public void DROTG() {
-		riSon.ROTD();
-		ROTG();
-	}
-	
-	public void DROTD() {
-		leSon.ROTG();
-		ROTD();
-		k.bal
-	}
 
 	public int height(){
 		if(isLeaf()){
@@ -89,21 +93,11 @@ public class Noeud<E extends Comparable<E>> {
 		return riSon.height() - leSon.height();
 	}
 	
-
-		return (1 + Math.max(height(riSon, leSon)));
-			return (1 + Math.max(height(riSon, leSon)));
-		}
-	}
-	
-	public int balance(){
-		return riSon.height() - leSon.height();
-	}
 	public int getBalance(){
 		return bal;
 	}
 	
-		public Noeud<E> ROTD() {
-	public Noeud<E> ROTD() {
+		public void ROTD() {
 		Noeud<E> k = leSon; 
 		int a = bal; 
 		int b = k.getBalance();
@@ -111,8 +105,69 @@ public class Noeud<E extends Comparable<E>> {
 		k.riSon = this; /*Rotation*/
 		setBal(a-Math.max(b,0)-1);
 		k.setBal(Math.min(a-2,a+b-2,b-1);
-		return k;
+	}
+	public void ROTG() {
+		Noeud k = riSon; //b <- fils droit
+		int a = balance(); 
+		int b = k.balance();
+		riSon = k.getLeSon();
+		k.leSon = this; /*Rotation*/
+		setBal(a-Math.max(b, 0)-1);
+		k.setBal(Math.min(a-2, Math.min(a+b, b-1))); //La noeud droit de l'objet est devenu racine.
 	}
 	
+	public void DROTG() {
+		riSon.ROTD();
+		ROTG();
+	}
+	
+	public void DROTD() {
+		leSon.ROTG();
+		ROTD();
+
+	}
+	
+	public void equilibrer(){
+		if(bal ==2 ){
+			if(riSon.bal >=0){
+				ROTG();
+			}else{
+				riSon  = riSon.ROTD();
+				ROTG();
+			}
+		}else if(bal == -2){
+			if(leSon.bal >=0){
+				ROTD();
+			}else{
+				leSon  = leSOn.ROTD();
+				ROTD();
+			}
+		}
+	}
+	
+	public int supp(E x, int variationH){  // A MODIFIER COMME ADD
+		if(x>etq){
+			riSon.supp(x, variationH);
+		}else if(x<etq){
+			leSon.supp(x, -variationH);
+		}else if(leSon == null)
+			//variation = -1
+		}else if(rison == null){
+			// variation = -1
+		}else{
+			etq = mini(rison);
+			riSon.oterMin(); // vrairation H prend variation de otermin
+		}
+		
+		if(variationH != 0){
+			bal = bal + variationH;
+			equilibrer();
+			if(bal == 0){
+				//variationH = -1
+		}
+	}
+	
+	public int oterMin(){ //A FAIRE COMME ADD
+	}
 		
 }
